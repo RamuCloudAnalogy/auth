a7242eee9af86762987811bf64c7f3e5be8db8a0
ramudalli <dalli.ramu@cloudanalogy.com>
2020-05-28 01:09:21 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    google: {
+        clientID: '72670751843-jultqv3db8dv9kn71d26g99fp7kor6v4.apps.googleusercontent.com',
+        clientSecret: 'W0yhpub4ti0Zo1rFl3OjkkR3'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://test3333:sfdc3333@cluster0-gof2c.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session:{
+        cookieKey:'thenetninjaisawsomeiguess'
+    }
+};
+
